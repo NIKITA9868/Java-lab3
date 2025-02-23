89ccaf2b768eebf8f827d52b1924bc31e68a3530
Nikita <nikitas7268@gmail.com>
2025-02-23 17:29:42 +0300

    Добавлено все

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -4,8 +4,11 @@ import com.example.demo.dto.ResponseGameDto;
 import com.example.demo.service.GameService;
 import java.util.HashMap;
 import java.util.Map;
-
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
